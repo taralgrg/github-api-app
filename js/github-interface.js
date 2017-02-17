d80d647b6c9294e7c34399f6641a2db67f3d7aff
Mac <taralgurung@hotmail.com>
2017-02-17 17:47:54 +0800

    Final version 2

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,15 +2,12 @@ var apiKey = "6577eae8560bcb6250532cbc0e62b1d08e643dc4";
 
 
 $(document).ready(function() {
-  // console.log("hi");
   $('#submit').click(function(event) {
     event.preventDefault();
     var username = $('#body-entry').val();
-    // console.log("click");
     $('#body-entry').val("");
-    // console.log(username);
     $.get("https://api.github.com/users/"+username+'/repos'+"?access_token="+apiKey).then(function(response){
-      console.log(response);
+
       var arr = response;
       $.each (arr,function (i,v) {
         $.each(v, function( key, value ) {
@@ -21,8 +18,7 @@ $(document).ready(function() {
 
  }).fail(function(error){
 
-      console.log(error);
+
     });
-    // console.log("gets to the end");
   });
 });
